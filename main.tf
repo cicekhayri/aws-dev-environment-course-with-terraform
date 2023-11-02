5d9005c15de3317b332e1320d49641bc5603626b
Hayri Cicek <hayri.cicek@hiq.se>
2023-11-02 10:53:09 +0100

    Fixes lint

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ terraform {
   required_version = ">= 1.6.2"
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = ">= 5.23.1"
+      source  = "hashicorp/aws"
+      version = ">= 5.23.1"
     }
   }
 }
 
 variable "aws_region" {
-  type = string
+  type    = string
   default = "us-east-1"
 }
 
@@ -18,17 +18,17 @@ provider "aws" {
 }
 
 data "archive_file" "myzip" {
-    type = "zip"
-    source_file = "main.py"
-    output_path = "main.zip"
+  type        = "zip"
+  source_file = "main.py"
+  output_path = "main.zip"
 }
 
 resource "aws_lambda_function" "mypython_lambda" {
-  filename = "main.zip"
-  function_name = "mypython_lambda_test"
-  role = aws_iam_role.mypython_lambda_role.arn
-  handler = "main.lambda_handler"
-  runtime = "python3.11"
+  filename         = "main.zip"
+  function_name    = "mypython_lambda_test"
+  role             = aws_iam_role.mypython_lambda_role.arn
+  handler          = "main.lambda_handler"
+  runtime          = "python3.11"
   source_code_hash = "data.archive_file.myzip.output_base64sha256"
 }
 
@@ -48,21 +48,4 @@ data "aws_iam_policy_document" "assume_role" {
 resource "aws_iam_role" "mypython_lambda_role" {
   name               = "mypython_role"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
-resource "aws_sqs_queue" "main_queue" {
-  name = "my-main-queue"
-  delay_seconds = 30
-  max_message_size = 262144
-}
-
-resource "aws_sqs_queue" "dlq_queue" {
-  name = "my-dlq-queue"
-  delay_seconds = 30
-  max_message_size = 262144
-}
-
-resource "aws_lambda_event_source_mapping" "sqs-lambda-trigger" {
-  event_source_arn = aws_sqs_queue.main_queue.arn
-  function_name = aws_lambda_function.mypython_lambda.arn
 }
